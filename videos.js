7d30514af3484b470cd6fc009012d24daaf3f377
tiagotrabalho <druzyktrabalho@gmail.com>
2024-04-25 13:02:32 -0300

    Primeiro commit

---
+++ videos.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    src: "./img/08.mp4",
+    title: "EPISÓDIO 1",
+  },
+  {
+    src: "./img/02.mp4",
+    title: "Live Stream",
+  },
+  {
+    src: "./img/03.mp4",
+    title: "Digitando",
+  },
+  {
+    src: "./img/04.mp4",
+    title: "Home office",
+  },
+  {
+    src: "./img/05.mp4",
+    title: "Likes",
+  },
+  {
+    src: "./img/06.mp4",
+    title: "Gota de água",
+  },
+  {
+    src: "./img/07.mp4",
+    title: "Código",
+  },
+  {
+    src: "./img/03.mp4",
+    title: "Digitando",
+  },
+  {
+    src: "./img/04.mp4",
+    title: "Home office",
+  },
+  {
+    src: "./img/05.mp4",
+    title: "Likes",
+  },
+  {
+    src: "./img/06.mp4",
+    title: "Gota de água",
+  },
+  {
+    src: "./img/07.mp4",
+    title: "Código",
+  },
+];
